8f4d65910662d7d4e0d6e4b105daa80e937f8709
MrLYC <imyikong@gmail.com>
2022-03-31 10:56:47 +0800

    feat: add helper command in demo

--- sdks/apigw-manager/demo/settings.py
+++ sdks/apigw-manager/demo/settings.py
@@ -130,6 +130,13 @@ USE_TZ = env.bool("USE_TZ", default=True)
 
 STATIC_URL = "/static/"
 
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
