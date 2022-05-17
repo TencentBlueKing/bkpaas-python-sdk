c93b92e56bdc10370e38c6ff6be5537feec2e7f9
nannan00 <17491932+nannan00@users.noreply.github.com>
2022-05-17 09:13:24 +0800

    feat(web str error): support lazy str, such as django i18n gettext

--- sdks/blue-krill/blue_krill/web/std_error.py
+++ sdks/blue-krill/blue_krill/web/std_error.py
@@ -57,7 +57,8 @@ class APIError(Exception):
         if replace:
             obj_message = message
         else:
-            obj_message = ''.join([self._message, self.delimiter, message])
+            # use map(str, ..) to compatibility with lazy loaded messages，such as django i18n gettext
+            obj_message = ''.join(map(str, [self._message, self.delimiter, message]))
         obj_message = self._render(obj_message, kwargs)
         return self._clone(message=obj_message)
 
