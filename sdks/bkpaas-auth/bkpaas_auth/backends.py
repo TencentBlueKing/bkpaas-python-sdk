72154c148e476bff4dcef664d5e5aeb4ea0b0b36
jiayuan929 <252461528@qq.com>
2023-04-13 20:32:53 +0800

    fix: 异常日志

--- sdks/bkpaas-auth/bkpaas_auth/backends.py
+++ sdks/bkpaas-auth/bkpaas_auth/backends.py
@@ -58,8 +58,11 @@ class UniversalAuthBackend:
             )
             token.user_info = UserInfo(username=username)
             logger.debug(f'New login token exchanged by credentials, token={login_token}')
-        except (ServiceError, InvalidTokenCredentialsError):
+        except InvalidTokenCredentialsError:
             logger.warning('Invalid credentials given, authenticate error')
+            return None
+        except ServiceError:
+            logger.warning('Error requesting third-party API service, authenticate error')
             return None
 
         return self.get_user_by_token(token)
