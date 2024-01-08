c0e87511fcf4adcf431a723490d898a0377ae925
dijiachen <dijiachen@tencent.com>
2024-01-08 15:28:51 +0800

    feat:添加接口，支持通过 name 查询 service_instance

--- sdks/paas-service/paas_service/views.py
+++ sdks/paas-service/paas_service/views.py
@@ -179,7 +179,7 @@ class SvcInstanceViewSet(viewsets.ViewSet):
 
     @m_verified_client_required
     def retrieve_by_fields(self, request, service_id):
-        """Retrieve an instance by name"""
+        """Retrieve an instance by fields"""
         qs = ServiceInstance.objects.filter(service__uuid=service_id)
         # 由于 name 在 credentials 加密字段中， 所以不能直接使用 filter 过滤
         # 只能遍历所有 ServiceInstance 来查找
