d892ba2ad069c714b80b82e14361db6f7481e188
wklken <wklken@gmail.com>
2024-07-23 15:21:27 +0800

    test(apigw-manager/drf): add more tests

--- sdks/apigw-manager/tests/apigw_manager/drf/test_authentication.py
+++ sdks/apigw-manager/tests/apigw_manager/drf/test_authentication.py
@@ -7,10 +7,33 @@
 * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 * specific language governing permissions and limitations under the License.
 """
+from typing import Optional
 
 import pytest
 from unittest.mock import Mock
 from apigw_manager.drf.authentication import ApiGatewayJWTAuthentication
+from apigw_manager.apigw.providers import PublicKeyProvider
+
+
+class MockEmptyProvider(PublicKeyProvider):
+    def provide(self, gateway_name: str, jwt_issuer: Optional[str] = None) -> Optional[str]:
+        return None
+
+class MockJwtProvider(PublicKeyProvider):
+    def provide(self, gateway_name: str, jwt_issuer: Optional[str] = None) -> Optional[str]:
+        jwt = Mock()
+        jwt.payload = {
+            "app": {"app_code": "mock_app", "verified": True},
+            "user": {"username": "mock_user"},
+                        }
+        jwt.gateway_name = "mock_gateway"
+
+        return jwt
+
+
+@pytest.fixture()
+def mock_request(mocker):
+    return mocker.MagicMock()
 
 
 class TestApiGatewayJWTAuthentication:
@@ -26,3 +49,30 @@ class TestApiGatewayJWTAuthentication:
 
         assert app.bk_app_code == "my_app"
         assert app.verified
+
+    @pytest.fixture(autouse=True)
+    def _patch_authenticate(self, mocker):
+        self.authenticate_function = mocker.patch("django.contrib.auth.authenticate")
+
+    def test_get_user(self):
+        self.authenticate_function.return_value = "my_user"
+        user = ApiGatewayJWTAuthentication().get_user(Mock())
+        assert user == "my_user"
+
+    def test_authenticate_invalid_provider(self):
+        authentication = ApiGatewayJWTAuthentication()
+        authentication.provider = MockEmptyProvider("mock")
+
+        assert authentication.authenticate(Mock()) is None
+
+    def test_authenticate(self, mock_request):
+        self.authenticate_function.return_value = "my_user"
+
+        authentication = ApiGatewayJWTAuthentication()
+        authentication.provider = MockJwtProvider("mock")
+
+        user, arg2 = authentication.authenticate(mock_request)
+        assert arg2 is None
+        assert user == "my_user"
+
+        assert mock_request.app == authentication.make_app(bk_app_code="mock_app", verified=True)
