f65cc4da31dd067bc368431c004405c2ea82a3d0
schnee <suzh9@mail2.sysu.edu.cn>
2024-10-22 10:52:06 +0800

    feat: paas-service support py3.11

--- sdks/paas-service/paas_service/__init__.py
+++ sdks/paas-service/paas_service/__init__.py
@@ -16,6 +16,6 @@ limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
-__version__ = '1.1.6'
+__version__ = '2.0.0'
 
 default_app_config = 'paas_service.apps.PaaSServiceConfig'
