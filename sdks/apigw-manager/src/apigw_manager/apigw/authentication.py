100b22dcf267681357fef99585d75931524caa8f
wklken <wklken@gmail.com>
2025-04-22 16:11:46 +0800

    fix(apigw/authentication.py): fix missing tenant_id in UserBackend (#221)

--- sdks/apigw-manager/src/apigw_manager/apigw/authentication.py
+++ sdks/apigw-manager/src/apigw_manager/apigw/authentication.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 """
- * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-蓝鲸 PaaS 平台(BlueKing-PaaS) available.
- * Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
+* TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-蓝鲸 PaaS 平台(BlueKing-PaaS) available.
+* Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
+* Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at http://opensource.org/licenses/MIT
+* Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+* an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations under the License.
 """
+
 import logging
 from collections import namedtuple
 from typing import ClassVar, Type
@@ -162,7 +163,7 @@ class UserModelBackend(ModelBackend):
         user.tenant_id = ""  # type: ignore
         return user
 
-    def authenticate(self, request, gateway_name, bk_username, tenant_id, verified, **credentials):
+    def authenticate(self, request, gateway_name, bk_username, verified, tenant_id="", **credentials):
         if not verified:
             return self.make_anonymous_user(bk_username=bk_username)
 
