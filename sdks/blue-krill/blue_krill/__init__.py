adf688a4496520546ef033180b5e3a428c5d1e35
piglei <piglei2007@gmail.com>
2025-08-13 14:42:31 +0800

    chore: bump blue-krill to 2.1.3

--- sdks/blue-krill/blue_krill/__init__.py
+++ sdks/blue-krill/blue_krill/__init__.py
@@ -15,4 +15,4 @@
 # We undertake not to change the open source license (MIT license) applicable
 # to the current version of the project delivered to anyone in the future.
 
-__version__ = "2.1.2"
+__version__ = "2.1.3"
