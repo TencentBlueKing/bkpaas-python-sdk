2981c579fa413e0f2ecd5b1ea3c66044c6787d54
xogas <57179186+xogas@users.noreply.github.com>
2026-06-03 15:40:18 +0800

    chore(blue-krill): update copyright entity in source file headers

--- sdks/blue-krill/blue_krill/async_utils/poll_task.py
+++ sdks/blue-krill/blue_krill/async_utils/poll_task.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # TencentBlueKing is pleased to support the open source community by making
 # 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
-# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) Tencent. All rights reserved.
 # Licensed under the MIT License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
 #
@@ -14,7 +14,6 @@
 #
 # We undertake not to change the open source license (MIT license) applicable
 # to the current version of the project delivered to anyone in the future.
-
 
 import logging
 import time
--- sdks/blue-krill/blue_krill/auth/client.py
+++ sdks/blue-krill/blue_krill/auth/client.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # TencentBlueKing is pleased to support the open source community by making
 # 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
-# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Copyright (C) Tencent. All rights reserved.
 # Licensed under the MIT License (the "License"); you may not use this file except
 # in compliance with the License. You may obtain a copy of the License at
 #
@@ -91,7 +91,7 @@ class JWTClientAuthenticator:
                 algorithm = client.get("algorithm", DEFAULT_ALGORITHM)
                 payload = jwt.decode(token, client["key"], algorithms=[algorithm])
             except DecodeError:
-                logger.debug(f'Unable to decode token using {client["iss"]}\'s secret')
+                logger.debug(f"Unable to decode token using {client['iss']}'s secret")
                 continue
 
             if not self._validate_payload(client, payload):
