a0a951950b07efbecc1d3b1fd202cdb72c372341
xogas <57179186+xogas@users.noreply.github.com>
2026-06-04 15:48:50 +0800

    chore(paas-service): update copyright entity in source file headers

--- sdks/paas-service/paas_service/auth/decorator.py
+++ sdks/paas-service/paas_service/auth/decorator.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-TencentBlueKing is pleased to support the open source community by making
-蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
-Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except
-in compliance with the License. You may obtain a copy of the License at
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
+# Copyright (C) Tencent. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
 
-    http://opensource.org/licenses/MIT
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-either express or implied. See the License for the specific language governing permissions and
-limitations under the License.
-
-We undertake not to change the open source license (MIT license) applicable
-to the current version of the project delivered to anyone in the future.
-"""
 import logging
 from collections.abc import Mapping
 from functools import wraps
@@ -35,10 +34,10 @@ def instance_authorized_require(func):
 
     @wraps(func)
     def wrapper(request, instance_id, *args, **kwargs):
-        authorized_instances = request.session.get('authorized_instances', {})
+        authorized_instances = request.session.get("authorized_instances", {})
         if not isinstance(authorized_instances, Mapping):
-            logger.error('authorized_instances type error')
-            return HttpResponseServerError('internal server error, please try again')
+            logger.error("authorized_instances type error")
+            return HttpResponseServerError("internal server error, please try again")
 
         # Check if instance has been authorized
         if instance_id not in authorized_instances:
@@ -52,7 +51,7 @@ def instance_authorized_require(func):
 def verified_client_required(func):
     @wraps(func)
     def wrapper(request, *args, **kwargs):
-        client = getattr(request, 'client', None)
+        client = getattr(request, "client", None)
         if client and client.is_verified():
             return func(request, *args, **kwargs)
         else:
@@ -68,7 +67,7 @@ def verified_client_role_require(role_name):
     def _wrapper(func):
         @wraps(func)
         def _wrapped(request, *args, **kwargs):
-            client = getattr(request, 'client', None)
+            client = getattr(request, "client", None)
             if client and client.is_verified() and role_name == client.role:
                 return func(request, *args, **kwargs)
             else:
--- sdks/paas-service/tests/test_auth.py
+++ sdks/paas-service/tests/test_auth.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-TencentBlueKing is pleased to support the open source community by making
-蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
-Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
-Licensed under the MIT License (the "License"); you may not use this file except
-in compliance with the License. You may obtain a copy of the License at
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
+# Copyright (C) Tencent. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
 
-    http://opensource.org/licenses/MIT
-
-Unless required by applicable law or agreed to in writing, software distributed under
-the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-either express or implied. See the License for the specific language governing permissions and
-limitations under the License.
-
-We undertake not to change the open source license (MIT license) applicable
-to the current version of the project delivered to anyone in the future.
-"""
 import time
 
 import jwt
@@ -24,7 +23,7 @@ from django.test.utils import override_settings
 from paas_service.auth.backends import InstanceAuthBackend, InstanceAuthFailed
 
 pytestmark = pytest.mark.django_db
-PAAS_SERVICE_JWT_CLIENTS = [{'iss': 'c1', 'key': 'foobar', 'algorithm': 'HS256'}]
+PAAS_SERVICE_JWT_CLIENTS = [{"iss": "c1", "key": "foobar", "algorithm": "HS256"}]
 
 
 class TestInstanceAuthBackend:
@@ -36,8 +35,8 @@ class TestInstanceAuthBackend:
 
     @override_settings(PAAS_SERVICE_JWT_CLIENTS=PAAS_SERVICE_JWT_CLIENTS)
     def test_token_invalid(self, rf, instance):
-        payload = {'iss': 'c1', 'service_instance_id': str(instance.uuid), 'expires_at': time.time() + 3600}
-        token = jwt.encode(payload, key='invald-secret', algorithm='HS256')
+        payload = {"iss": "c1", "service_instance_id": str(instance.uuid), "expires_at": time.time() + 3600}
+        token = jwt.encode(payload, key="invald-secret", algorithm="HS256")
         request = rf.request(QUERY_STRING=f"token={token}")
         request.session = {}
 
@@ -46,8 +45,8 @@ class TestInstanceAuthBackend:
 
     @override_settings(PAAS_SERVICE_JWT_CLIENTS=PAAS_SERVICE_JWT_CLIENTS)
     def test_expired_token(self, rf, instance):
-        payload = {'iss': 'c1', 'service_instance_id': str(instance.uuid), 'expires_at': time.time() - 3600}
-        token = jwt.encode(payload, key='foobar', algorithm='HS256')
+        payload = {"iss": "c1", "service_instance_id": str(instance.uuid), "expires_at": time.time() - 3600}
+        token = jwt.encode(payload, key="foobar", algorithm="HS256")
         request = rf.request(QUERY_STRING=f"token={token}")
         request.session = {}
 
@@ -56,8 +55,8 @@ class TestInstanceAuthBackend:
 
     @override_settings(PAAS_SERVICE_JWT_CLIENTS=PAAS_SERVICE_JWT_CLIENTS)
     def test_normal_token(self, rf, instance):
-        payload = {'iss': 'c1', 'service_instance_id': str(instance.uuid), 'expires_at': time.time() + 3600}
-        token = jwt.encode(payload, key='foobar', algorithm='HS256')
+        payload = {"iss": "c1", "service_instance_id": str(instance.uuid), "expires_at": time.time() + 3600}
+        token = jwt.encode(payload, key="foobar", algorithm="HS256")
         request = rf.request(QUERY_STRING=f"token={token}")
         request.session = {}
 
