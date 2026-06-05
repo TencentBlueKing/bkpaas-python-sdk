3c3b1954e07748ec9f9020b500c4a13cdf80ebe9
xogas <57179186+xogas@users.noreply.github.com>
2026-06-05 11:20:12 +0800

    chore(bkapi-component-open): update copyright entity in source file headers (#285)

--- sdks/bkapi-component-open/bkapi_component/open/client.py
+++ sdks/bkapi-component-open/bkapi_component/open/client.py
@@ -1,13 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
- * TencentBlueKing is pleased to support the open source community by making 蓝鲸智云-蓝鲸 PaaS 平台(BlueKing-PaaS) available.
- * Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
- * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://opensource.org/licenses/MIT
- * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations under the License.
-"""
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
+# Copyright (C) Tencent. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+
 from bkapi_client_core.esb import ESBClient, Operation, OperationGroup, bind_property
 
 
@@ -1531,6 +1538,7 @@ class CmsiGroup(OperationGroup):
         path="/api/c/compapi{bk_api_ver}/cmsi/send_wecom_robot/",
     )
     # --- 以上api只在te上云版可用 ---
+
 
 class GseGroup(OperationGroup):
     # Agent心跳信息查询
@@ -3138,6 +3146,7 @@ class UsermanageGroup(OperationGroup):
 
 class Client(ESBClient):
     """ESB Components"""
+
     bk_login = bind_property(BkLoginGroup, name="bk_login")
     cc = bind_property(CcGroup, name="cc")
     cmsi = bind_property(CmsiGroup, name="cmsi")
